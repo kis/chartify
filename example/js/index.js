8f91db11b0d3a38617ae9b33e84436376036c403
Kirill Stepkin <kirillstyopkin@gmail.com>
2016-11-17 17:27:06 +0200

    update demo

--- example/js/index.js
+++ example/js/index.js
@@ -18,11 +18,13 @@ class App extends Component {
 			items: votes.map(item => ({				
 				value: item["моя оценка"],
 				title: item["русскоязычное название"]
-			}))
+			})),
+			boxSize: 16,
+			theme: 'purple'
 		});
 	}
 
-	handleClick = () => {
+	changeChart = () => {
 		const { items } = this.state;
 		let newItems = items.map(item => {
 			item.sortValue = Math.random().toFixed(5);
@@ -33,22 +35,57 @@ class App extends Component {
 		});
 	}
 
+	changeRange = () => {
+		let val = document.getElementById("range").value;
+		this.setState({
+			boxSize: val
+		});
+	}
+
+	changeTheme = () => {
+		const themes = {0:'default', 1:'purple', 2:'grey'};
+		let curr = 0;
+
+		Object.values(themes).forEach((val, i) => {
+			if (this.state.theme == val) {
+				curr = i;
+			}
+		});
+
+		let next = curr < Object.keys(themes).length - 1 ? curr + 1 : 0;
+
+		this.setState({
+			theme: themes[next]
+		});
+	}
+
 	render() {
-		const { items } = this.state;
+		const { items, boxSize, theme } = this.state;
 		return (
 			<div className="container">
+				<input id="range" type="range" min="0" max="20" step="1" onChange={this.changeRange} /> 
+
 				<Chartify 
 					data = {items}
 					width = {50} 					      
 					height = {10}
-					boxSize = {16}
+					boxSize = {boxSize}
 					line = {true}
-					theme = {"purple"} />
+					theme = {theme} />
+
 				<button 
 					type="button" 
 					className="button"
-					onClick={this.handleClick}
-				>Change chart</button>
+					onClick={this.changeChart}>
+					Change chart
+				</button>
+
+				<button 
+					type="button" 
+					className="button"
+					onClick={this.changeTheme}>
+					Change theme
+				</button>
 			</div>
 		);
 	}
