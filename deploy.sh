866acc24e8fbfa82916f23a96fd0f5a3375fcf5c
Kirill Stepkin <kirillstyopkin@gmail.com>
2016-11-27 12:17:56 +0200

    deploy

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+# http://www.damian.oquanta.info/posts/one-line-deployment-of-your-site-to-gh-pages.html
+
+git checkout -b gh-pages
+git commit -am "init"
+git push origin gh-pages
+git checkout master
+git push origin `git subtree split --prefix example gh-pages`:gh-pages --force
+
+# every push
+
+git subtree push --prefix example origin gh-pages
