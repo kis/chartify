dcc958ea236b3bcc00f848ef63dbbf433f9d2cc3
Kirill Stepkin <kirillstyopkin@gmail.com>
2017-02-20 11:51:47 +0200

    update

--- src/Chartify.js
+++ src/Chartify.js
@@ -17,27 +17,6 @@ export default class Chartify extends Component {
 		super(props);
 	}
 
-	getMarkStyle(config) {
-		let {
-			box_size = 20, 
-			box_radius = 8, 
-			bordered = false
-		} = config;
-
-		const markStyle = {
-			'width': `${box_size}px`,
-			'height': `${box_size}px`,
-			'borderRadius': `${box_radius}px`
-		};
-
-		if (!bordered) {
-			markStyle['borderTop'] = 'transparent';
-			markStyle['borderLeft'] = 'transparent';
-		}
-
-		return markStyle;
-	}
-
 	renderRow(mark: Mark, markNum: number, row: Array) {
 		let { data } = this.props;
 
@@ -49,31 +28,49 @@ export default class Chartify extends Component {
 			blink = false
 		} = this.props.config;
 
-		let markStyle = this.getMarkStyle(this.props.config);
+		let styles = this.getStyles(this.props.config);
 
 		return (
 			<div>
 				{row.map(i => {
-					let markClass = null;
+					let markClasses = this.getMarkClasses(height, mark, i);
+					let markStyles = this.getMarkStyles(styles, markClasses, blink);
+					let isActiveMark = height - mark.value == i.value;
 
-					if (height - mark.value > i.value) markClass = "mark empty";
-					if (height - mark.value == i.value) markClass = "mark";
-					if (height - mark.value < i.value) markClass = "mark painted";
-
-					let isPoint = height - mark.value == i.value && markNum < data.length - 1;
-
-					let style = {...markStyle};
-
-					if (markClass == "mark painted" && blink) {
-						style = {...markStyle, 'animation': 'blink 0.5s infinite'};
-					}
-
-					return <div key={i.value} style={style} className={markClass}>
-						{isPoint ? this.renderMarkTools(mark, markNum, line || line_only) : null}
+					return <div key={i.value} style={markStyles} className={markClasses}>
+						{isActiveMark ? this.renderMarkTools(mark, markNum, line || line_only) : null}
 					</div>
 				})}
 			</div>
 		)
+	}
+
+	getMarkClasses(height: number, mark: Mark, i: Object) {
+		if (height - mark.value > i.value) return "mark empty";
+		if (height - mark.value == i.value) return "mark";
+		if (height - mark.value < i.value) return "mark painted";
+	}
+
+	getStyles(config: Object) {
+		let {
+			box_size = 20, 
+			box_radius = 8, 
+			bordered = false
+		} = config;
+
+		return {
+			'width': `${box_size}px`,
+			'height': `${box_size}px`,
+			'borderRadius': `${box_radius}px`,
+			'borderTop': !bordered ? 'transparent' : 'none',
+			'borderLeft': !bordered ? 'transparent' : 'none'
+		};
+	}
+
+	getMarkStyles(styles: Object, markClasses: string, blink: boolean) {
+		return (markClasses == "mark painted" && blink) ? 
+			{...styles, 'animation': 'blink 0.5s infinite'} :
+			{...styles};
 	}
 
 	renderMarkTools(mark: Mark, markNum: number, drawLine: boolean) {
@@ -83,23 +80,27 @@ export default class Chartify extends Component {
 		return (
 			<div>
 				{drawLine ? <div className="line" style={lineStyle}></div> : null}
-				<div className="tooltiptext">
-					<div className="value">{mark.value}</div>
-					<div>{mark.title}</div>
-					<div className="date">{mark.date}</div>
-				</div>
+				{this.renderTooltip(mark)}
 			</div>
 		);
 	}
 
-	calcLineStyle(currentMark: number, nextMark: number) {
+	renderTooltip(mark: Mark) {
+		return (
+			<div className="tooltiptext">
+				<div className="value">{mark.value}</div>
+				<div>{mark.title}</div>
+				<div className="date">{mark.date}</div>
+			</div>
+		);
+	}
+
+	calcLineStyles(currentMark: number, nextMark: number) {
 		const { box_size = 20 } = this.props.config;
 		const AC = box_size;
 		const BC = Math.abs(nextMark - currentMark) * box_size;
 		const AB = Math.hypot(AC, BC);
-		let angleA = Math.fround(Math.asin( BC / AB ) * 180 / Math.PI);
-
-		if (nextMark > currentMark) angleA = -angleA;
+		let angleA = this.calcLineAngle(BC, AB, nextMark, currentMark);
 
 		return {
 			width: `${AB}px`,
@@ -107,6 +108,12 @@ export default class Chartify extends Component {
 			top: `${parseInt(box_size/2)}px`,
 			left: `${parseInt(box_size/2)}px`
 		};
+	}
+
+	calcLineAngle(BC: number, AB: number, nextMark: number, currentMark: number) {
+		let angleA = Math.fround(Math.asin( BC / AB ) * 180 / Math.PI);
+		if (nextMark > currentMark) angleA = -angleA;
+		return angleA;
 	}
 
 	renderYAxis(row: Array) {
@@ -124,14 +131,8 @@ export default class Chartify extends Component {
 	}
 
 	render() {
-		let {
-			data = []
-		} = this.props;
-
-		let {
-			height = 10,
-			theme = 'default'
-		} = this.props.config;
+		let {data = []} = this.props;
+		let {height = 10, theme = 'default'} = this.props.config;
 
 		const row = Array(height).fill().map((item, i) => ({ value: i }));
 		const rulerClass = `ruler-container ${theme}`;
---
+++ tests/chartify.test.js
@@ -0,0 +1,5 @@
+const Chartify = require('../src/Chartify.js');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
