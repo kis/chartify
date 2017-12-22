38b071c071385feb4a249ef37576362d6ff40bed
Kirill Stepkin <kirillstyopkin@gmail.com>
2017-12-22 15:46:26 +0200

    add prettier

--- src/Chartify.js
+++ src/Chartify.js
@@ -1,162 +1,175 @@
 // @flow
-import React, { Component, Fragment } from 'react';
-import Draggable from './Draggable';
-import _ from 'underscore';
-import './chartify.css';
+import React, { Component, Fragment } from "react";
+import Draggable from "./Draggable";
+import _ from "underscore";
+import "./chartify.css";
 
 type Props = {};
 type Mark = {
-	title: string,
-	x_value: string,
-	y_value: number
+  title: string,
+  x_value: string,
+  y_value: number
 };
 
 export default class Chartify extends Component {
-	renderRow(mark: Mark, markNum: number, row: Array, maxX: number) {
-		let { data } = this.props;
+  renderRow(mark: Mark, markNum: number, row: Array, maxX: number) {
+    let { data } = this.props;
 
-		let {
-			height = 10,
-			line = false,
-			line_only = false,
-			bordered = false,
-			blink = false
-		} = this.props.config;
+    let {
+      height = 10,
+      line = false,
+      line_only = false,
+      bordered = false,
+      blink = false
+    } = this.props.config;
 
-		let styles = this.getStyles(this.props.config);
+    let styles = this.getStyles(this.props.config);
 
-		mark.chart_y_value = Math.round((mark.y_value * height) / maxX);
-		mark.chart_y_value = mark.chart_y_value ? mark.chart_y_value : 1;
+    mark.chart_y_value = Math.round(mark.y_value * height / maxX);
+    mark.chart_y_value = mark.chart_y_value ? mark.chart_y_value : 1;
 
-		return (
-			<Fragment>
-				{row.map(i => {
-					let markClasses = line_only ? 'mark white' : this.getMarkClasses(height, mark, i);
-					let markStyles = this.getMarkStyles(styles, markClasses, blink);
-					let isActiveMark = height - mark.chart_y_value == i.y_value && markNum < data.length - 1;
+    return (
+      <Fragment>
+        {row.map(i => {
+          let markClasses = line_only
+            ? "mark white"
+            : this.getMarkClasses(height, mark, i);
+          let markStyles = this.getMarkStyles(styles, markClasses, blink);
+          let isActiveMark =
+            height - mark.chart_y_value == i.y_value &&
+            markNum < data.length - 1;
 
-					return <div key={i.y_value} style={markStyles} className={markClasses}>
-						{isActiveMark ? this.renderMarkTools(mark, markNum, line || line_only) : null}
-					</div>
-				})}
-			</Fragment>
-		)
-	}
+          return (
+            <div key={i.y_value} style={markStyles} className={markClasses}>
+              {isActiveMark
+                ? this.renderMarkTools(mark, markNum, line || line_only)
+                : null}
+            </div>
+          );
+        })}
+      </Fragment>
+    );
+  }
 
-	getMarkClasses(height: number, mark: Mark, i: Object) {
-		if (height - mark.chart_y_value > i.y_value) return "mark empty";
-		if (height - mark.chart_y_value == i.y_value) return "mark active";
-		if (height - mark.chart_y_value < i.y_value) return "mark painted";
-	}
+  getMarkClasses(height: number, mark: Mark, i: Object) {
+    if (height - mark.chart_y_value > i.y_value) return "mark empty";
+    if (height - mark.chart_y_value == i.y_value) return "mark active";
+    if (height - mark.chart_y_value < i.y_value) return "mark painted";
+  }
 
-	getStyles(config: Object) {
-		let {
-			box_size = 20,
-			box_radius = 8,
-			bordered = false
-		} = config;
+  getStyles(config: Object) {
+    let { box_size = 20, box_radius = 8, bordered = false } = config;
 
-		return {
-			'width': `${box_size}px`,
-			'height': `${box_size}px`,
-			'borderRadius': `${box_radius}px`,
-			'borderTop': !bordered ? 'transparent' : '1px solid rgba(249,250,249, 0.9)',
-			'borderLeft': !bordered ? 'transparent' : '1px solid rgba(249,250,249, 0.9)'
-		};
-	}
+    return {
+      width: `${box_size}px`,
+      height: `${box_size}px`,
+      borderRadius: `${box_radius}px`,
+      borderTop: !bordered ? "transparent" : "1px solid rgba(249,250,249, 0.9)",
+      borderLeft: !bordered ? "transparent" : "1px solid rgba(249,250,249, 0.9)"
+    };
+  }
 
-	getMarkStyles(styles: Object, markClasses: string, blink: boolean) {
-		return (markClasses == "mark painted" && blink) ?
-			{...styles, 'animation': 'blink 0.5s infinite'} :
-			{...styles};
-	}
+  getMarkStyles(styles: Object, markClasses: string, blink: boolean) {
+    return markClasses == "mark painted" && blink
+      ? { ...styles, animation: "blink 0.5s infinite" }
+      : { ...styles };
+  }
 
-	renderMarkTools(mark: Mark, markNum: number, drawLine: boolean) {
-		let { data } = this.props;
-		let lineStyle = drawLine ? this.calcLineStyles(mark.chart_y_value, data[markNum + 1].chart_y_value) : null;
+  renderMarkTools(mark: Mark, markNum: number, drawLine: boolean) {
+    let { data } = this.props;
+    let lineStyle = drawLine
+      ? this.calcLineStyles(mark.chart_y_value, data[markNum + 1].chart_y_value)
+      : null;
 
-		return (
-			<Fragment>
-				{drawLine ? <div className="line" style={lineStyle}></div> : null}
-				{this.renderTooltip(mark)}
-			</Fragment>
-		);
-	}
+    return (
+      <Fragment>
+        {drawLine ? <div className="line" style={lineStyle} /> : null}
+        {this.renderTooltip(mark)}
+      </Fragment>
+    );
+  }
 
-	renderTooltip(mark: Mark) {
-		let tooltipStyle = {
-			top: (mark.chart_y_value < this.props.config.height / 2) ? '-100px' : 0
-		};
+  renderTooltip(mark: Mark) {
+    let tooltipStyle = {
+      top: mark.chart_y_value < this.props.config.height / 2 ? "-100px" : 0
+    };
 
-		return (
-			<div className="tooltiptext" style={tooltipStyle}>
-				<div className="value">{mark.y_value}</div>
-				<div>{mark.title}</div>
-				<div className="date">{mark.x_value}</div>
-			</div>
-		);
-	}
+    return (
+      <div className="tooltiptext" style={tooltipStyle}>
+        <div className="value">{mark.y_value}</div>
+        <div>{mark.title}</div>
+        <div className="date">{mark.x_value}</div>
+      </div>
+    );
+  }
 
-	calcLineStyles(currentMark: number, nextMark: number) {
-		const { box_size = 20 } = this.props.config;
-		const AC = box_size;
-		const BC = Math.abs(nextMark - currentMark) * box_size;
-		const AB = Math.hypot(AC, BC);
-		let angleA = this.calcLineAngle(BC, AB, nextMark, currentMark);
+  calcLineStyles(currentMark: number, nextMark: number) {
+    const { box_size = 20 } = this.props.config;
+    const AC = box_size;
+    const BC = Math.abs(nextMark - currentMark) * box_size;
+    const AB = Math.hypot(AC, BC);
+    let angleA = this.calcLineAngle(BC, AB, nextMark, currentMark);
 
-		return {
-			width: `${AB}px`,
-			transform: `rotate(${angleA}deg)`,
-			top: `${parseInt(box_size/2)}px`,
-			left: `${parseInt(box_size/2)}px`
-		};
-	}
+    return {
+      width: `${AB}px`,
+      transform: `rotate(${angleA}deg)`,
+      top: `${parseInt(box_size / 2)}px`,
+      left: `${parseInt(box_size / 2)}px`
+    };
+  }
 
-	calcLineAngle(BC: number, AB: number, nextMark: number, currentMark: number) {
-		let angleA = Math.fround(Math.asin( BC / AB ) * 180 / Math.PI);
-		if (nextMark > currentMark) angleA = -angleA;
-		return angleA;
-	}
+  calcLineAngle(BC: number, AB: number, nextMark: number, currentMark: number) {
+    let angleA = Math.fround(Math.asin(BC / AB) * 180 / Math.PI);
+    if (nextMark > currentMark) angleA = -angleA;
+    return angleA;
+  }
 
-	renderYAxis(row: Array, maxValue: number) {
-		return (
-			<div className="y-axis-wrapper">
-				<div className="y-axis">
-					{row.map(i => {
-						return <div className="y-caption" key={i.y_value}>
-							{i.y_value % 2 == 0 ? maxValue - i.y_value : null}
-						</div>
-					})}
-				</div>
-			</div>
-		);
-	}
+  renderYAxis(row: Array, maxValue: number) {
+    return (
+      <div className="y-axis-wrapper">
+        <div className="y-axis">
+          {row.map(i => {
+            return (
+              <div className="y-caption" key={i.y_value}>
+                {i.y_value % 2 == 0 ? maxValue - i.y_value : null}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
 
-	render() {
-		let {data = []} = this.props;
-		let {height = 10, theme = 'default'} = this.props.config;
+  render() {
+    let { data = [] } = this.props;
+    let { height = 10, theme = "default" } = this.props.config;
 
-		let maxValueObj = _.max(this.props.data, el => { return el.y_value; });
-		let maxValue = maxValueObj.y_value;
+    let maxValueObj = _.max(this.props.data, el => {
+      return el.y_value;
+    });
+    let maxValue = maxValueObj.y_value;
 
-		const row = Array(height).fill().map((item, i) => {
-			return {
-				y_value: i*(maxValue/height)
-			};
-		});
+    const row = Array(height)
+      .fill()
+      .map((item, i) => {
+        return {
+          y_value: i * (maxValue / height)
+        };
+      });
 
-		const rulerClass = `ruler-container ${this.props.container} ${theme}`;
+    const rulerClass = `ruler-container ${this.props.container} ${theme}`;
 
-		return (
-			<div className={rulerClass}>
-				{this.renderYAxis(row, maxValue)}
-				<Draggable
-					data={data}
-					maxX={maxValue}
-					container={this.props.container}
-					config={this.props.config}
-					renderRow={this.renderRow.bind(this)} />
-			</div>
-		);
-	}
+    return (
+      <div className={rulerClass}>
+        {this.renderYAxis(row, maxValue)}
+        <Draggable
+          data={data}
+          maxX={maxValue}
+          container={this.props.container}
+          config={this.props.config}
+          renderRow={this.renderRow.bind(this)}
+        />
+      </div>
+    );
+  }
 }
--- src/Draggable.js
+++ src/Draggable.js
@@ -1,141 +1,151 @@
 // @flow
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 type Props = {};
 
 export default class Draggable extends Component {
-	componentDidMount() {
-		let { data = [] } = this.props;
-		let container = document.querySelector(`.${this.props.container}`);
+  componentDidMount() {
+    let { data = [] } = this.props;
+    let container = document.querySelector(`.${this.props.container}`);
 
-		this.elements = {
-			inner: container.querySelector('.marks'),
-			outer: container.querySelector('.marks-wrapper'),
-			xAxis: container.querySelector('.x-axis')
-		};
+    this.elements = {
+      inner: container.querySelector(".marks"),
+      outer: container.querySelector(".marks-wrapper"),
+      xAxis: container.querySelector(".x-axis")
+    };
 
-		let {innerRightPos, outerRightPos} = this.getPos();
+    let { innerRightPos, outerRightPos } = this.getPos();
 
-		this.lastDelta = outerRightPos - innerRightPos;
+    this.lastDelta = outerRightPos - innerRightPos;
 
-		let newVal = `translateX(${this.lastDelta}px)`;
-		this.elements.inner.style.transform = newVal;
-		this.elements.xAxis.style.transform = data.length ? newVal : 0;
-	}
+    let newVal = `translateX(${this.lastDelta}px)`;
+    this.elements.inner.style.transform = newVal;
+    this.elements.xAxis.style.transform = data.length ? newVal : 0;
+  }
 
-	startDrag = e => {
-		this.pageX = e.pageX;
-		let lastTransform = this.elements.inner.style.transform;
+  startDrag = e => {
+    this.pageX = e.pageX;
+    let lastTransform = this.elements.inner.style.transform;
 
-		if (!lastTransform) {
-			this.lastDelta = 0;
-		} else {
-			let re = /\D(-?\d+)\D/g;
-			let execVal = re.exec(lastTransform);
-			this.lastDelta = Number(execVal[1]);
-		}
+    if (!lastTransform) {
+      this.lastDelta = 0;
+    } else {
+      let re = /\D(-?\d+)\D/g;
+      let execVal = re.exec(lastTransform);
+      this.lastDelta = Number(execVal[1]);
+    }
 
-		this.checkMove = true;
-	}
+    this.checkMove = true;
+  };
 
-	processDrag = e => {
-		let {innerPos, outerPos, innerRightPos, outerRightPos} = this.getPos();
+  processDrag = e => {
+    let { innerPos, outerPos, innerRightPos, outerRightPos } = this.getPos();
 
-		if (this.lastDelta == 0) this.rightState = outerRightPos - innerRightPos;
-		if (!this.checkMove || innerPos > outerPos || innerRightPos < outerRightPos) return;
+    if (this.lastDelta == 0) this.rightState = outerRightPos - innerRightPos;
+    if (!this.checkMove || innerPos > outerPos || innerRightPos < outerRightPos)
+      return;
 
-		let deltaX = e.pageX - this.pageX;
-		let newDelta = this.lastDelta + deltaX;
+    let deltaX = e.pageX - this.pageX;
+    let newDelta = this.lastDelta + deltaX;
 
-		if (innerRightPos + deltaX < outerRightPos) newDelta = this.rightState;
-		if (innerPos + deltaX > outerPos) newDelta = 0;
+    if (innerRightPos + deltaX < outerRightPos) newDelta = this.rightState;
+    if (innerPos + deltaX > outerPos) newDelta = 0;
 
-		this.lastDelta = newDelta;
-		let newVal = `translateX(${newDelta}px)`;
-		this.elements.inner.style.transform = newVal;
-		this.elements.xAxis.style.transform = newVal;
-	}
+    this.lastDelta = newDelta;
+    let newVal = `translateX(${newDelta}px)`;
+    this.elements.inner.style.transform = newVal;
+    this.elements.xAxis.style.transform = newVal;
+  };
 
-	drop = e => { this.checkMove = false; }
+  drop = e => {
+    this.checkMove = false;
+  };
 
-	renderMarks(marksStyle) {
-		const { height } = this.props.config;
-		const row = Array(height).fill().map((item, i) => ({ y_value: i }));
-		let maxX = this.props.maxX;
+  renderMarks(marksStyle) {
+    const { height } = this.props.config;
+    const row = Array(height)
+      .fill()
+      .map((item, i) => ({ y_value: i }));
+    let maxX = this.props.maxX;
 
-		return (
-			<div className="marks"
-				 style={marksStyle}
-				 onTouchStart={this.startDrag.bind(this)}
-				 onTouchMove={this.processDrag.bind(this)}
-				 onTouchEnd={this.drop.bind(this)}
-				 onMouseDown={this.startDrag.bind(this)}
-				 onMouseMove={this.processDrag.bind(this)}
-				 onMouseUp={this.drop.bind(this)}>
-				{this.props.data.map((mark, markNum) => (
-					<div className="ruler-row" key={markNum}>
-						{this.props.renderRow(mark, markNum, row, maxX)}
-					</div>
-				))}
-			</div>
-		);
-	}
+    return (
+      <div
+        className="marks"
+        style={marksStyle}
+        onTouchStart={this.startDrag.bind(this)}
+        onTouchMove={this.processDrag.bind(this)}
+        onTouchEnd={this.drop.bind(this)}
+        onMouseDown={this.startDrag.bind(this)}
+        onMouseMove={this.processDrag.bind(this)}
+        onMouseUp={this.drop.bind(this)}
+      >
+        {this.props.data.map((mark, markNum) => (
+          <div className="ruler-row" key={markNum}>
+            {this.props.renderRow(mark, markNum, row, maxX)}
+          </div>
+        ))}
+      </div>
+    );
+  }
 
-	renderXAxis(marksStyle) {
-		const { box_size = 20 } = this.props.config;
-		const { data: marks = 50 } = this.props;
-		let showDateCount = 0;
+  renderXAxis(marksStyle) {
+    const { box_size = 20 } = this.props.config;
+    const { data: marks = 50 } = this.props;
+    let showDateCount = 0;
 
-		marks.forEach((mark, markNum) => {
-			showDateCount = markNum % 10 == 0 ? ++showDateCount : showDateCount;
-		});
+    marks.forEach((mark, markNum) => {
+      showDateCount = markNum % 10 == 0 ? ++showDateCount : showDateCount;
+    });
 
-		let width = parseInt(marks.length * box_size / showDateCount);
-		let style = { 'width': `${width}px` };
+    let width = parseInt(marks.length * box_size / showDateCount);
+    let style = { width: `${width}px` };
 
-		return (
-			<div className="x-axis" style={marksStyle}>
-				{marks.map((mark, markNum) => (
-					markNum % 10 == 0 ?
-						<div className="x-caption" style={style} key={markNum}>
-							{mark.x_value}
-						</div> : null
-				))}
-			</div>
-		);
-	}
+    return (
+      <div className="x-axis" style={marksStyle}>
+        {marks.map(
+          (mark, markNum) =>
+            markNum % 10 == 0 ? (
+              <div className="x-caption" style={style} key={markNum}>
+                {mark.x_value}
+              </div>
+            ) : null
+        )}
+      </div>
+    );
+  }
 
-	getPos() {
-		const { box_size = 20 } = this.props.config;
-		const { data: marks = [] } = this.props;
+  getPos() {
+    const { box_size = 20 } = this.props.config;
+    const { data: marks = [] } = this.props;
 
-		let windowScrollX = window.scrollX;
-		let chartLength = marks.length;
-		let innerPos = this.elements.inner.getBoundingClientRect().left - windowScrollX;
-		let outerPos = this.elements.outer.getBoundingClientRect().left - windowScrollX;
-		let innerRightPos = innerPos + chartLength * box_size;
-		let outerRightPos = outerPos + this.elements.outer.offsetWidth;
+    let windowScrollX = window.scrollX;
+    let chartLength = marks.length;
+    let innerPos =
+      this.elements.inner.getBoundingClientRect().left - windowScrollX;
+    let outerPos =
+      this.elements.outer.getBoundingClientRect().left - windowScrollX;
+    let innerRightPos = innerPos + chartLength * box_size;
+    let outerRightPos = outerPos + this.elements.outer.offsetWidth;
 
-		return {
-			innerPos,
-			outerPos,
-			innerRightPos,
-			outerRightPos
-		};
-	}
+    return {
+      innerPos,
+      outerPos,
+      innerRightPos,
+      outerRightPos
+    };
+  }
 
-	render() {
-		const { box_size = 20 } = this.props.config;
-		const { data: marks } = this.props;
-		let width = marks.length * box_size;
-		let marksStyle = { 'width': `${width || 750}px` };
+  render() {
+    const { box_size = 20 } = this.props.config;
+    const { data: marks } = this.props;
+    let width = marks.length * box_size;
+    let marksStyle = { width: `${width || 750}px` };
 
-		return (
-			<div className="marks-wrapper">
-				{this.renderMarks(marksStyle)}
-				{this.renderXAxis(marksStyle)}
-			</div>
-		);
-	}
-
+    return (
+      <div className="marks-wrapper">
+        {this.renderMarks(marksStyle)}
+        {this.renderXAxis(marksStyle)}
+      </div>
+    );
+  }
 }
